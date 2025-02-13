7cb5372e492134e00b75492534594879e191c19f
Nael Dahman <nael_d@hotmail.com>
2025-02-14 01:02:21 +0300

    fixed issue #5 by Crow owner but still need to avoid using `disableCodeMinifying()`

--- src/Crow.php
+++ src/Crow.php
@@ -370,6 +370,7 @@ class Crow {
 
     $code = str_replace(["\t", "\n", "\r"], ' ', $code);
     $code = preg_replace('/\s{2,}/m', ' ', $code);
+    $code = str_replace('?> <?php', '', $code);
     $code = trim($code);
   }
 }
